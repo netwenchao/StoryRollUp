504b793ee209299d293d05ddae3a528c657cc91d
netwenchao <netwenchao@hotmail.com>
2013-05-26 23:42:41 +0800

    add
    Signed-off-by: netwenchao <netwenchao@hotmail.com>

--- Joke/src/com/netwc/joke/JokeDailyActivity.java
+++ Joke/src/com/netwc/joke/JokeDailyActivity.java
@@ -1,5 +1,14 @@
 package com.netwc.joke;
 
+import java.util.HashMap;
+
+import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -12,4 +21,15 @@ public class JokeDailyActivity extends Activity{
 		setContentView(R.layout.activity_daily_list);
 	}
 	
+	public HashMap<String,String> GetDailyJoke() throws ParserException{
+		HashMap<String,String> dailyJoke=new HashMap<String,String>();
+		final String dailyUrl="http://www.jokeji.cn/";
+		Parser htmlParse=new Parser(dailyUrl);
+		htmlParse.setEncoding("gb2312");
+		NodeList newsContainer=htmlParse.extractAllNodesThatMatch(new AndFilter(new TagNameFilter("div"),new HasAttributeFilter("class", "newcontent l_left")));
+		if(newsContainer!=null && newsContainer.size()>0){
+			
+		}
+		return dailyJoke;
+	}
 }
