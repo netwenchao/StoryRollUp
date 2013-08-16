3c07ab23941ffe09cae19aee46db60144793fcb0
netwenchao <netwenchao@hotmail.com>
2013-08-16 18:12:18 +0800

    at Signed-off-by: netwenchao <netwenchao@hotmail.com>

--- MedBooks/src/com/luckywc/Data/PageContentAdapter.java
+++ MedBooks/src/com/luckywc/Data/PageContentAdapter.java
@@ -1,12 +1,31 @@
 package com.luckywc.Data;
 
+import com.luckywc.medbooks.R;
+
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class PageContentAdapter extends PagerAdapter{
+	private Context mContext;
+	private Cursor mCursor;	
 	
-	private Cursor mCursor;
+	public PageContentAdapter(Context context,Cursor cursor){
+		mContext=context;
+		mCursor=cursor;
+	}
+	
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		// TODO Auto-generated method stub
+		//return super.instantiateItem(container, position);
+		LayoutInflater inflater=(LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View dView= inflater.inflate(R.layout.activity_detail,container);
+		return container;
+	}	
 	
 	@Override
 	public int getCount() {
